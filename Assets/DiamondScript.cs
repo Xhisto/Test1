8bb52c49c2df5393085d31e1b8686256723e5a61
CripC <Alrik-Mark.Brillantes@elev.ga.lbs.se>
2018-10-02 09:18:58 +0200

    First Github

---
+++ Assets/DiamondScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondScript : MonoBehaviour
+{
+    public int MyValue;
+    public SpriteRenderer rend;
+    public Color newColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend.color = newColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
